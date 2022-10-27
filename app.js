7215669e60051b460c02d9ba2045f5fe9dc07fb7
rahman-habib <97686740+rahman-habib@users.noreply.github.com>
2022-10-26 22:23:27 -0700

    update

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
 const express = require('express');
-const Run = require('run-sdk');
+//const Run = require('run-sdk');
 const SupplyChain = require('./supplychain');
 //using Express methods
 const app = express();
 app.use(express.json({ extended: true }));
 require("dotenv").config();
+app.use(function (req, res, next) {
+    //  SAMPLE res.header("Access-Control-Allow-Origin", "http://192.168.1.100:8025"); // update to match the domain you will make the request from
+    res.header("Access-Control-Expose-Headers", "cooljwt");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    );
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    next();
+  });
 
-const run = new Run({
-    network: "test",
-    owner: process.env.OWNER,
-    purse: process.env.PURSE,
-})
+// const run = new Run({
+//     network: "test",
+//     owner: process.env.OWNER,
+//     purse: process.env.PURSE,
+// })
 const newSupply = new SupplyChain();
 module.exports = newSupply;
 console.log("SupplyChain smartContract Initialized!");
