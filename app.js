c9f5d4195419ab9887d85c812a14583a996507b8
rahman-habib <habib.rahman4665@gmail.com>
2022-12-29 15:28:19 +0500

    update

--- app.js
+++ app.js
@@ -8,12 +8,8 @@ var express = require("express");
 var app = express();
 
 //Use Methods
-app.use(bodyParser.json());
-app.use(
-    bodyParser.urlencoded({
-        extended: true,
-    })
-);
+app.use(bodyParser.json({limit: "50mb"}));
+app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header(
